4cd1f0ee17f2f29791b7e211b1d3983189af2e2c
burtonwilliamt <burtonwilliamt@users.noreply.github.com>
2022-06-11 03:27:01 -0400

    Forgoten work on nosferatu

---
+++ src/nosferatu/custom_http.py
@@ -0,0 +1,63 @@
+import aiohttp
+import asyncio
+
+
+url = "https://discord.com/api/v8/applications/<my_application_id>/commands"
+
+# This is an example CHAT_INPUT or Slash Command, with a type of 1
+json = {
+    "name": "blep",
+    "type": 1,
+    "description": "Send a random adorable animal photo",
+    "options": [
+        {
+            "name": "animal",
+            "description": "The type of animal",
+            "type": 3,
+            "required": True,
+            "choices": [
+                {
+                    "name": "Dog",
+                    "value": "animal_dog"
+                },
+                {
+                    "name": "Cat",
+                    "value": "animal_cat"
+                },
+                {
+                    "name": "Penguin",
+                    "value": "animal_penguin"
+                }
+            ]
+        },
+        {
+            "name": "only_smol",
+            "description": "Whether to show only baby animals",
+            "type": 5,
+            "required": False
+        }
+    ]
+}
+
+# For authorization, you can use either your bot token
+headers = {
+    "Authorization": "Bot <my_bot_token>"
+}
+
+# or a client credentials token for your app with the applications.commands.update scope
+headers = {
+    "Authorization": "Bearer <my_credentials_token>"
+}
+
+# r = requests.post(url, headers=headers, json=json)
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        async with session.get('http://python.org') as response:
+            html = await response.text()
+            print(html)
+
+
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
